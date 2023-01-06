1196e1f7d16c60a9553b0168b2fa89c082701e0e
erikgustavo00 <erikgustavo00@gmail.com>
2023-01-06 20:37:31 -0300

    first commit

---
+++ src/notifications/Notification.jsx
@@ -0,0 +1,12 @@
+import './Notification.scss';
+
+function Notification(){
+  return(
+    <div className='notification-body'>
+      <h2>Notifications 3</h2>
+      <p>Mark all as read</p>
+    </div>
+  )
+}
+
+export default Notification;
