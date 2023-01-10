63cf9a25a7a571bedb5039b7f8116d4d3d2906f4
erikgustavo00 <erikgustavo00@gmail.com>
2023-01-10 17:36:28 -0300

    10/01

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import './App.scss';
 import Notification from './notifications/Notification.jsx';
 import Notification_single from './notifications-sigle/Notication_single';
-
+import Images from './images/Images';
+import useState from 'react';
 
 function App() {
-  var img = '<img src="./images/avatar-angela-gray"/>'
+  
   
   return (
     <div className="App">
       <div className='app-container'>
         <Notification>
-          <Notification_single   name='mark webber' text='reacted to your recent post' post='My first tournament today!' time='1m ago'/>
-         
-          <Notification_single name='Angela Gray' text='followed you' time='5m ago'/>
+        <button className='button--activated'>
+          <Notification_single style={{background:'red'}} img={Images.mark} name='mark webber' text='reacted to your recent post' post='My first tournament today!' time='1m ago'/>
+        </button>
+        <button>
+          <Notification_single img={Images.angela} name='Angela Gray' text='followed you' time='5m ago'/>
+        </button>
+          <button>
+          <Notification_single img={Images.jacob} name='Jacob Thompson' text='has joined your group' group='Chess Club' time='1 day ago'/>
+          </button>
+          <button>
+          <Notification_single img={Images.rizky}  name='Rizky Hasanuddin' text='sent you a private message' time=' 5 days ago' messageClass='message' message="Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
+            I'm already having lots of fun and improving my game."/>
+            </button>
+          <button>
+          <Notification_single img={Images.kimberly} name='Kimberly Smith' text='commented on your picture' chess='chess' time='1 week ago'/>
+          </button>
+          <button>
+          <Notification_single img={Images.nathan} name='Nathan Peterson' text='reacted to your recent post' post='5 end-game strategies to increase your win rate' time='2 weeks ago'/>
+          </button>
+          <button>
+          <Notification_single img={Images.anna} name='Anna Kim'  text='left the group' group='Chess Club' time='2 weeks ago'/>
+          </button>
+           
         </Notification>
           
         
--- src/images/Images.js
+++ src/images/Images.js
@@ -1,6 +1,12 @@
-const Images = [
+const Images = 
     {
-        'mark': './avatar-mark-webber.webp',
-        angela: './avatar-angela-gray'
+        mark: require("./avatar-mark-webber.webp") ,
+        angela: require("./avatar-angela-gray.webp"),
+        jacob:require("./avatar-jacob-thompson.webp"),
+        rizky:require('./avatar-rizky-hasanuddin.webp'),
+        kimberly: require('./avatar-kimberly-smith.webp'),
+        nathan: require('./avatar-nathan-peterson.webp'),
+        anna: require("./avatar-anna-kim.webp")
     }
-]
+
+export default Images;
