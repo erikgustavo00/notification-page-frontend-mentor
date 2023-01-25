e954d2e6e823ddb062d1141274dc80ddb87fd16e
erikgustavo00 <erikgustavo00@gmail.com>
2023-01-25 19:19:31 -0300

    last commit

---
+++ src/Users.js
@@ -0,0 +1,98 @@
+export default [
+  
+  {
+    "id": '1',
+    "author":{
+        "img": require("./images/avatar-mark-webber.webp"),
+        "name": "Mark Webber",
+        "href": "#"
+      },
+    "text": "reacted to your recent post",
+    "link":{
+        "post": "My first tournament today!",
+        "href": "#"
+      },
+    "time": "1m ago",
+    "isUnread": true
+  },
+  {
+    "id": '2',
+    "author":{
+      "img": require("./images/avatar-angela-gray.webp"),
+      "name": "Angela Gray","href":"#"
+    },
+      "text": "followed you",
+      "time": "5m ago",
+    "isUnread": true,
+  },
+  {
+    "id": '3',
+    "author":{
+      "img":require("./images/avatar-jacob-thompson.webp"),
+      "name": "Jacob Thompson","href":"#"
+      },
+    "text": "has joined your group",
+    "link":{
+        "group": "Chess Club",
+        "href": "#"
+        },
+    "time": "1 day ago",
+    "isUnread": true,
+},
+  {
+    "id": '4',
+    "author":{
+      "img":require('./images/avatar-rizky-hasanuddin.webp'),
+      "name": "Rizky Hasanuddin","href":"#"
+    },
+      "text": "sent you a private message",
+      "time": " 5 days ago",
+      "messageClass": "message",
+      "message": `Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and 
+              I'm already having lots of fun and improving my game.`,
+      "isUnread": false
+},
+  {
+  "id": '5',
+  "author":{
+    "img": require('./images/avatar-kimberly-smith.webp'),
+    "name": "Kimberly Smith","href":"#"
+  },
+  "text": "commented on your picture",
+  "chess": "chess",
+  "link":{
+    "URL": "url('./images/image-chess.webp')",
+    "href": "#"
+  },
+   "time": "1 week ago",
+   "isUnread": false
+  },
+  {
+  "id": '6',
+  "author":{
+    "img": require('./images/avatar-nathan-peterson.webp'),
+    "name": "Nathan Peterson","href":"#"
+  },
+  "text": "reacted to your recent post",
+  "post": "5 end-game strategies to increase your win rate",
+  "time": "2 weeks ago",
+  "isUnread": false
+},
+  {
+  "id": '7',
+  "author":{
+    "img": require("./images/avatar-anna-kim.webp"),
+    "name": "Anna Kim",
+    "href":"#"
+  },
+    "text": "left the group",
+    
+    "link":{
+      "group": "Chess Club",
+      "href":"#"
+      },
+    "time": "2 weeks ago",
+    "isUnread":false
+},
+  
+];
---
+++ src/react-components/notifications/Notification.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import './Notification.scss';
+import Users from '../../Users'
+import { useState } from 'react'
+
+function Notification(props){
+  const [users,setUsers] = useState(Users)
+
+function setAllUnread(){
+  setUsers((prev) => prev.map(user => ({...user, isUnread: false })))
+}
+
+function hadleNotificationClick(id){
+  setUsers((prev)=> prev.map(user => (user.id === id ? {...user, isUnread:false }: user)))
+}
+
+  function getUser(){
+ 
+    return (users && users.map((user)=>(
+     
+     
+        <div>  
+
+      <button type='button' key={user.id} onClick={()=>hadleNotificationClick(user.id)} className={user.isUnread && 'button--activated'}>
+           
+           <div className='notification_single'>
+           <div className="img">
+            <a href="#">
+             <img src={user.author.img} alt="avatar" />
+             </a>
+           </div>
+           <div className="notify-content">
+             <div className="text">
+               <span className="name">
+                 <a href={user.author.href}>{user.author.name}</a>
+               </span>
+               <span className="text">{user.text}</span>
+               <span className="group">
+               {user.link && (<a href={user.link.href}>{user.link.group}</a>)}
+               </span>
+               <span>{user.link && (<a href={user.link.href}>{user.link.post}</a>)}</span><span className={user.isUnread && 'redSignal'}></span>
+             </div>{/* text*/}
+             <span className="time">{user.time}</span>
+             {user.link && <a href={user.link.href} className={user.chess}>{user.secondary_img}</a>}
+             
+             <div className={user.messageClass}>
+               <p>{user.message}</p>
+             </div>
+           </div>
+         </div>
+         
+       </button>
+       
+       </div>
+       ))
+     );}
+
+  
+  
+return(   
+    <div className='notification-body'>
+      <h2>Notifications <span className='isUnreadLength'>{users.filter(user => user.isUnread).length}</span></h2>
+      <button className='allUnread' onClick={()=>setAllUnread()}>Mark all as read</button>
+      <div className="clear"></div>
+      <div>{getUser()}</div>
+      
+    </div>
+  )
+}
+
+export default Notification;
